7d796107efb63b8b5d5d782bf482bef86fb27a42
kejsper\Programowanie <kejsper@gmail.com>
2017-08-04 21:22:05 +0200

    fixed styling, js working

--- build/js/script.js
+++ build/js/script.js
@@ -9,12 +9,14 @@ $(document).ready( () => {
   // sum of all cases
   $.get(getSumUrl, function(data) {
     $('.header__sum').html(data);
-    console.log('Sum loaded')
   });
 
   // top10 debts
   $.get(getTopDebts, function (data) {
-    dataToHtml(data);
+    var dataSorted = data.sort( function (a,b) {
+      return b.Value - a.Value;
+    });
+    dataToHtml(dataSorted);
   });
 
   // Data from API to html
@@ -22,22 +24,22 @@ $(document).ready( () => {
     $('.content__data').html('');
     $.each(data, function(key, value) {
       var htmledData = `
-      <div class="content__row">
+      <div class="content__row" data-div="#collapse${value.Id}">
         <div class="row" role="tab" id="heading${value.Id}">
-          <div class="col-5">${value.Name}</div>
-          <div class="col-3">${value.NIP}</div>
-          <div class="col-2">${value.Value}</div>
+          <div class="col-5"><p class="content__p">dłużnik</p><p class="content__value">${value.Name}</p></div>
+          <div class="col-3"><p class="content__p">nip</p><p class="content__value">${value.NIP}</p></div>
+          <div class="col-2"><p class="content__p">kwota zadłużenia</p><p class="content__value">${value.Value}</p></div>
           <div class="col-2">
-            <a href="#collapse${value.Id}" class="content__link" data-link="#collapse${value.Id}">
+            <a href="#" class="content__link" data-link="#collapse${value.Id}">
             więcej
             </a>
           </div>
         </div>
         <div id="collapse${value.Id}" class="row content__hidden">
-          <div class="col-5">${value.Address}</div>
-          <div class="col-3">${value.DocumentType}</div>
-          <div class="col-2">${value.Price}</div>
-          <div class="col-2">${value.Number}</div>
+          <div class="col-5"><p class="content__p">adres</p><p class="content__value content__value--more">${value.Address}</p></div>
+          <div class="col-3"><p class="content__p">typ dokumentu</p><p class="content__value content__value--more">${value.DocumentType}</p></div>
+          <div class="col-2"><p class="content__p">cena zadłużenia</p><p class="content__value content__value--more">${value.Price}</p></div>
+          <div class="col-2"><p class="content__p">numer</p><p class="content__value content__value--more">${value.Number}</p></div>
         </div>
       </div>
       `;
@@ -49,12 +51,26 @@ $(document).ready( () => {
   // accordion
 
   $('body').on('click', '.content__link', function() {
+    that = $(this);
     link = $(this).data('link');
 
     if (! $(link).hasClass('content__hidden--not')) {
+
+      that.html('mniej');
+      // changing background color for wrapper
+      $('[data-div]').removeClass('content__row--shadow');
+      $(`[data-div = "${link}"]`).addClass('content__row--shadow');
+      // showing additional info
       $('#accordion').find('.content__hidden--not').removeClass('content__hidden--not');
       $(link).addClass('content__hidden--not');
+      // showing hints
+      $('#accordion').find('.content__p--show').removeClass('content__p--show');
+      $(`[data-div = "${link}"]`).find('.content__p').addClass('content__p--show');
+
     } else {
+      that.html('więcej');
+      $(`[data-div = "${link}"]`).find('.content__p--show').removeClass('content__p--show');
+      $(`[data-div = "${link}"]`).removeClass('content__row--shadow');
       $('#accordion').find('.content__hidden--not').removeClass('content__hidden--not');
     }
   });
